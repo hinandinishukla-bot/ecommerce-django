3a6a72bbea228b78f7294dabf8e7b1264ad75813
Nandini Shukla <hinandinishukla@gmail.com>
2026-07-04 23:17:03 +0530

    Updated login.html

--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Medicine(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    expiry_date = models.DateField()
+
+    def __str__(self):
+        return self.name
--- store/views.py
+++ store/views.py
@@ -1,4 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+def login_page(request):
+
+    if request.method == "POST":
+
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+
+        else:
+            messages.error(request, "Invalid Username or Password")
+
+    return render(request, "login.html")
+
 
 def home(request):
+
+    if not request.user.is_authenticated:
+        return redirect("login")
+
     return render(request, "home.html")
+
+
+def logout_page(request):
+    logout(request)
+    return redirect("login")
