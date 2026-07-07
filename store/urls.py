bcb0f7430c22173f2edd845fa36566b1ddea286a
Nandini Shukla <hinandinishukla@gmail.com>
2026-07-07 23:58:20 +0530

    Updated login.html db

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-     path('login/', views.login_page, name='login'),positively-yours
-] 
+    # The name='login' matches redirect('login')
+    path('', views.login_page, name='login'), 
+    
+    # The name='home' matches redirect('home')
+    path('home/', views.home, name='home'),   
+    
+    path('logout/', views.logout_page, name='logout'),
+]
